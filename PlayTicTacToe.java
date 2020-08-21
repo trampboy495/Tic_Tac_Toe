80b3e39e118fa7baab3e3bc18a1065255ff2f4ee
trampboy495 <histroylover@gmail.com>
2020-08-21 22:37:28 +0530

    added: coin toss to decide who play first

--- PlayTicTacToe.java
+++ PlayTicTacToe.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class PlayTicTacToe {
+	private static Player currentPlayer;
+	private static Scanner sc = new Scanner(System.in);
+	private static int isComputer = 1;
+
+	PlayTicTacToe() {
+		init();
+	}
+
+	public void init() {
+		System.out.println("simulating coin toss to decide who plays first");
+		Random rand = new Random();
+		int random = rand.nextInt(2);
+	}
+
 	public static void main(String[] args) {
 		System.out.println("welcome to Tic Tac Toe game");
+		new PlayTicTacToe();
 	}
 }
