1888bd27f613c1175066be488d2036efc52b1272
trampboy495 <histroylover@gmail.com>
2020-08-23 15:47:21 +0530

    modified: to take side if center not available

--- PlayTicTacToe.java
+++ PlayTicTacToe.java
@@ -7,8 +7,10 @@ public class PlayTicTacToe {
 	private static Player human;
 	private static Scanner sc = new Scanner(System.in);
 	private static int isComputer = 1;
-	private static int cornerRow;
-	private static int cornerCol;
+	private static int cornerRow = -1;
+	private static int cornerCol = -1;
+	private static int sideRow = -1;
+	private static int sideCol = -1;
 	private static Board board;
 	private static GameState currentState;
 
@@ -34,25 +36,24 @@ public class PlayTicTacToe {
 		System.out.print("simulating coin toss to decide who plays first \n\n");
 		Random rand = new Random();
 		int random = rand.nextInt(2);
-		if(random == isComputer) {
-                        currentPlayer = Player.Cross;
+		if (random == isComputer) {
+			currentPlayer = Player.Cross;
 			computer = Player.Cross;
 			human = Player.Nought;
-                }
-                else {
-                        System.out.println("you won choose your symbol X or O");
-                        String choice = sc.nextLine();
-                        currentPlayer = (choice.equals("X")) ? (Player.Cross) : (Player.Nought);
+		} else {
+			System.out.println("you won choose your symbol X or O");
+			String choice = sc.nextLine();
+			currentPlayer = (choice.equals("X")) ? (Player.Cross) : (Player.Nought);
 			computer = (choice.equals("X")) ? (Player.Nought) : (Player.Cross);
 			human = currentPlayer;
-                }
+		}
 		currentState = GameState.Playing;
 		board.paint();
 
 	}
 
 	public void playMove(Player player) {
-		if(player == computer) {
+		if (player == computer) {
 			computerMove(player);
 		} else {
 			boolean validInput = false;
@@ -63,8 +64,7 @@ public class PlayTicTacToe {
 				if (row >= 0 && row < Board.ROWS && col >= 0 && col < Board.COLS
 						&& board.cells[row][col].content == Player.Empty) {
 					board.cells[row][col].content = player;
-					board.currentRow = row;
-					board.currentCol = col;
+					setCurrentRowAndCol(row, col);
 					validInput = true;
 				} else {
 					System.out.println("Invalid move at (" + row + "," + col + ") try again");
@@ -73,42 +73,61 @@ public class PlayTicTacToe {
 		}
 	}
 
+	public void setCurrentRowAndCol(int row, int col) {
+		board.currentRow = row;
+		board.currentCol = col;
+	}
+
 	public boolean getMove(Player player) {
 		int size = board.cells.length * board.cells.length;
 		int cornerCounter = 0;
-		for (int i=0; i < size; i++ ) {
-				int row = i / board.cells.length;
-				int col = i % board.cells.length;
-				if(board.cells[row][col].content == Player.Empty) {
-					board.cells[row][col].content = player;
-					board.currentRow = row;
-					board.currentCol = col;
-					if(board.hasWon(player))
-						return true;
-					board.cells[row][col].content = Player.Empty;
-					if(++row * ++col % 2 == 0 && row != 2 && col != 2 && cornerCounter < 1)
-						cornerRow = row - 1;
-						cornerCol = col - 1;
-						cornerCounter++;
+		int sideCounter = 0;
+		for (int i = 0; i < size; i++) {
+			int row = i / board.cells.length;
+			int col = i % board.cells.length;
+			if (board.cells[row][col].content == Player.Empty) {
+				board.cells[row][col].content = player;
+				setCurrentRowAndCol(row, col);
+				if (board.hasWon(player))
+					return true;
+				board.cells[row][col].content = Player.Empty;
+				if (++row * ++col != 4 && ((row * col) - 1) % 2 == 0 && cornerCounter < 1) {
+					cornerRow = row - 1;
+					cornerCol = col - 1;
+					cornerCounter++;
 				}
+				if (((row * col) - 1) % 2 != 0 && sideCounter < 1) {
+					sideRow = row - 1;
+					sideCol = col - 1;
+					sideCounter++;
+				}
+			}
 		}
 		return false;
 	}
 
 	public void computerMove(Player player) {
-		if(!getMove(player))
-			if(getMove(human))
+		if (!getMove(player)) {
+			if (getMove(human)) {
 				board.cells[board.currentRow][board.currentCol].content = player;
-			else if(cornerRow != 0)
+			} else if (cornerRow != -1) {
 				board.cells[cornerRow][cornerCol].content = player;
-			else if(board.cells[1][1].content == Player.Empty)
+				setCurrentRowAndCol(cornerRow, cornerCol);
+			} else if (board.cells[1][1].content == Player.Empty) {
 				board.cells[1][1].content = player;
+				setCurrentRowAndCol(1, 1);
+			} else if (sideRow != -1) {
+				board.cells[sideRow][sideCol].content = player;
+				setCurrentRowAndCol(sideRow, sideCol);
+			}
+		}
+		System.out.println("computer played its move");
 	}
 
 	public void updateGameStatus(Player player) {
-		if(board.hasWon(player)) {
+		if (board.hasWon(player)) {
 			currentState = (player == computer) ? (GameState.Computer_Won) : (GameState.Player_Won);
-		} else if(board.isDraw()) {
+		} else if (board.isDraw()) {
 			currentState = GameState.Draw;
 		}
 	}
